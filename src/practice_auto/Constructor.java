3249958455e93dad055550ecb45844f52458e53c
shwegowda22 <113420402+shwegowda22@users.noreply.github.com>
2022-09-23 12:06:49 +0530

    updated constructor
    added code which was missing

--- src/practice_auto/Constructor.java
+++ src/practice_auto/Constructor.java
@@ -1,9 +1,30 @@
 package practice_auto;
 
 public class Constructor {
+	
+	int x;
+	int y;
+	Constructor()
+	{
+		x=10;
+		y=30;
+	}
+	Constructor(int a,int b)
+	{
+		x=a;
+		y=b;
+	}
+	
+	void display()
+	{
+		System.out.println(x+y);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Constructor cm1= new Constructor();
+		cm1.display();
+		Constructor cm2= new Constructor(100,200);
+		cm2.display();
 
 	}
 
